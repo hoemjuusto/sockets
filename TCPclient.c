bd52d8cf92a8f920c0570a45fe0f0a40d8470cca
hoemjuusto <juuso.o.korhonen@aalto.fi>
2019-11-22 13:12:12 +0200

    added separate receiver

--- TCPclient.c
+++ TCPclient.c
@@ -12,6 +12,7 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <values.h>
+#include <fcntl.h>
 
 int main(){
 
@@ -25,13 +26,13 @@ int main(){
     server_address.sin_port = htons(9002);  // high number just to make sure that the port is open
     // IF CONNECTING BACK TO OWN SYSTEM
     server_address.sin_addr.s_addr = INADDR_ANY;
-
+    /*
     //OR IF CONNECTING TO REMOTE IP ADDRESS
-    /*struct in_addr addr;
+    struct in_addr addr;
     //Convert the IP dotted quad into struct in_addr
-    inet_aton("88.195.222.155", &(addr));
-    server_address.sin_addr.s_addr = addr.s_addr;*/
-
+    inet_aton("10.100.29.252", &(addr));
+    server_address.sin_addr.s_addr = addr.s_addr;
+    */
     int connection_status = connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address));
     if(connection_status == -1){
         perror("Error connecting to remote socket! \n\n");
@@ -45,22 +46,57 @@ int main(){
     int keepRunning = 1;
     char user_input[256];
 
-    while(keepRunning){
-        printf("\nGive serve-request (submit with enter, e to exit):\n");
-        fgets(user_input, 256, stdin);
-        char delim[] = "\n";
-        strcpy(user_input, strtok(user_input, delim));
-        printf("Sending the request: %s to server\n", user_input);
-        if(strcmp(user_input,"e") == 0)
-        {
-            printf("\n\nExiting the bank!\n\n");
-            send(network_socket, user_input, sizeof(user_input), 0);
-            keepRunning = 0;
-        }else{
-            send(network_socket, user_input, sizeof(user_input), 0);
-            recv(network_socket, &server_response, sizeof(server_response), 0);
-            printf("Server sent response: %s\n", server_response);
+    int fd[2];  //fd[0] for input, f[1] for output
+    pipe(fd);
+
+    char parent_message[100];
+
+    int pid;
+
+    pid = fork();
+
+
+
+    if(pid > 0) {
+
+        while (keepRunning) {
+            printf("\nGive serve-request (submit with enter, e to exit):\n");
+            fgets(user_input, 256, stdin);
+            char delim[] = "\n";
+            strcpy(user_input, strtok(user_input, delim));
+            printf("Sending the request: %s to server\n", user_input);
+            if (strcmp(user_input, "e") == 0) {
+                printf("\n\nExiting the bank!\n\n");
+                send(network_socket, user_input, sizeof(user_input), 0);
+                strcpy(parent_message, "e");
+                write(fd[1], parent_message, sizeof(parent_message));
+                keepRunning = 0;
+            } else {
+                send(network_socket, user_input, sizeof(user_input), 0);
+            }
         }
+    }else if(pid == 0){
+
+        close(fd[1]);
+
+        while(keepRunning) {
+
+
+            int flags = fcntl(fd[0], F_GETFL, 0);
+
+            fcntl(fd[0], F_SETFL, flags | O_NONBLOCK);  // to make read non blocking
+
+            if(read(fd[0], parent_message, 100)!=-1){
+                keepRunning = 0;
+            }
+
+            recv(network_socket, &server_response, sizeof(server_response), 0);
+            //printf("Server sent response: %s\n", server_response);
+        }
+        close(network_socket);
+        exit(0);
+    }else{
+
     }
 
     close(network_socket);
